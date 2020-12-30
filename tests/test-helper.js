dc970903bc8c123981533d77b4170745dfb85c5f
ijlee2 <ijlee2@users.noreply.github.com>
2020-12-30 08:51:39 -0600

    Upgraded ember-qunit to v5.1.1

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,10 @@ import Application from '@ember-learn/cli-guides/app';
 import config from '@ember-learn/cli-guides/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
